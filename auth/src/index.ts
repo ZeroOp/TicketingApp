d50e207844af508b1f91b6751530085e9727e8cb
Zeroop <jayaramnaik725@gmail.com>
2026-03-08 15:45:22 +0530

    Connect auth service with mongoDB container

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import { json } from 'body-parser';
+import  mongoose  from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { singinRouter } from './routes/signin';
 import { errorHandler } from './middlewares/error-handler';
-import { json } from 'body-parser';
 import { NotFoundError } from './errors/not-found-error';
 const app = express();
 app.use(json());
@@ -23,6 +24,17 @@ app.use(() => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Auth service listening on port 3000!!!!!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  }
+  catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('Auth service listening on port 3000');
+  });
+}
+
+start();
