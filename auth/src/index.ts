f34f25bc39ae92ae41c6a9e1bc9e4fa79a757602
Zeroop <jayaramnaik725@gmail.com>
2026-03-08 14:41:23 +0530

    Added custom-erros abstract class to make all the errors of similar type and reduced at error handlers file and added the not found error handling as well.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signoutRouter } from './routes/signout';
 import { singinRouter } from './routes/signin';
 import { errorHandler } from './middlewares/error-handler';
 import { json } from 'body-parser';
+import { NotFoundError } from './errors/not-found-error';
 const app = express();
 app.use(json());
 
@@ -12,12 +13,15 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 app.use(singinRouter);
-app.use(errorHandler);
 
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Auth ser-vice running 🚀' });
+// any route which is not found we are throughing the same error which is in the customError Format. 
+// made all the errors in the same format to make the client understand easily
+// if we have the async function then we need to use the next callback function like next(new NotFoundError()) instaead of using the throw new NotFoundError()
+app.use(() => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Auth service listening on port 3000!!!!!!!!!');
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-
+import { CustomError } from '../errors/custom-error';
 /*
     common error response type: 
     {
@@ -13,22 +11,11 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 */
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    
-    if( err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map((error) => {
-            console.log("I am error", error);
-            return {
-                message: error.msg,
-                field: (error as any).param || (error as any).path || 'unknown'
-            }
-        })
-        return res.status(400).send({ errors: formattedErrors });
+
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
 
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send( { errors: [{ message: err.reason }] } );
-    }
-    //  If there is a generic error, it should follow the same structure which we are following earlier. 
     res.status(400).send({
         errors: [
             {
