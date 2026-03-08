3e0f0df09a98ddcf2c10223aaafe92eaf48e21f5
Zeroop <jayaramnaik725@gmail.com>
2026-03-08 17:23:10 +0530

    Added Email in use error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+
 const router = express.Router();
 
 router.post('/api/users/signup' ,[
@@ -13,15 +15,27 @@ router.post('/api/users/signup' ,[
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ]
-    ,(req: Request, res: Response) => {
+    ,async (req: Request, res: Response) => {
+
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         throw new RequestValidationError(errors.array());
     }
 
-    console.log('Creating a user....');
-    throw new DatabaseConnectionError();
-    res.send('singup route');
+    const { email, password } = req.body; 
+
+    const existingUser = await User.findOne({
+        email
+    });
+
+    if (existingUser) {
+        throw new BadRequestError('Emain in use');
+    }
+    
+    const user = User.build({ email, password });
+    await user.save(); // we need to save the created user to the databaase. 
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter }
