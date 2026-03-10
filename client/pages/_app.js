ae12f5aea292fabcbea4d554090c8f11735da537
Zeroop <jayaramnaik725@gmail.com>
2026-03-10 19:59:18 +0530

    Added singout page and headers page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client';
+import Header from './components/header';
 
 // we have defined our own custom app component. when we route to either index.js or banana.js component what next does is will pass into the _app.js component. 
 // this is a some kind of wrapper
 const AppComponent = ({Component, pageProps, currentUser}) => {
     return (
         <div>
-            <h1>Header</h1>
+            <Header currentUser={currentUser} />
             <Component {...pageProps} currentUser={currentUser} />
         </div>
     )
