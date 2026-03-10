8f8905099f4a4e6d4fe5a0071e75d07041985bb0
Zeroop <jayaramnaik725@gmail.com>
2026-03-10 17:14:37 +0530

    Added most of logic to @zeroo-dev/common public package

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 // we have defined our own custom app component. when we route to either index.js or banana.js component what next does is will pass into the _app.js component. 
 // this is a some kind of wrapper
 const bootstrap = ({Component, pageProps}) => {
-    return <Component {...pageProps} />
+    return (
+        <div>
+            <h1>Header</h1>
+            <Component {...pageProps} />
+        </div>
+    )
 };
 
 export default bootstrap;
