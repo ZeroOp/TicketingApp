06a8b872751882475931edb4b412abdac02cadaa
Zeroop <jayaramnaik725@gmail.com>
2026-03-10 18:14:59 +0530

    Fixed few bugs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client';
 
 // we have defined our own custom app component. when we route to either index.js or banana.js component what next does is will pass into the _app.js component. 
 // this is a some kind of wrapper
-const bootstrap = ({Component, pageProps}) => {
+const AppComponent = ({Component, pageProps, currentUser}) => {
     return (
         <div>
             <h1>Header</h1>
-            <Component {...pageProps} />
+            <Component {...pageProps} currentUser={currentUser} />
         </div>
     )
 };
 
-export default bootstrap;
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+    const { data } = await client.get('/api/users/currentuser');
+
+    return {
+        pageProps,
+        currentUser: data.currentUser
+    };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -9,32 +9,32 @@ const LandingPage = ({ currentUser }) => {
     return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
-LandingPage.getInitialProps = async (context) => {
-    // req will be passed to the next.js 
-    // if (typeof window === 'undefined') {
-    //     // we are on the server!
-    //     // request should be made to http://ingress
-    //     const { data } = await axios.get(
-    //         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-    //             headers: req.headers
-    //         }
-    //     );
+// LandingPage.getInitialProps = async (context) => {
+//     // req will be passed to the next.js 
+//     // if (typeof window === 'undefined') {
+//     //     // we are on the server!
+//     //     // request should be made to http://ingress
+//     //     const { data } = await axios.get(
+//     //         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+//     //             headers: req.headers
+//     //         }
+//     //     );
 
-    //     return data;
-    // }
-    // else { 
-    //     // we are on the browser
-    //     // request can be made with a base url of ''
-    //     const {data} = await axios('/api/users/currentuser');
-    //     return data;
-    // }
+//     //     return data;
+//     // }
+//     // else { 
+//     //     // we are on the browser
+//     //     // request can be made with a base url of ''
+//     //     const {data} = await axios('/api/users/currentuser');
+//     //     return data;
+//     // }
 
-    // no need to do anything from above 
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
+//     // no need to do anything from above 
+//     const client = buildClient(context);
+//     const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-};
+//     return data;
+// };
 
 export default LandingPage;
 
