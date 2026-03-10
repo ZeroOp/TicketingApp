2c514879e665e9ad47c625bf9424a8866b638186
Zeroop <jayaramnaik725@gmail.com>
2026-03-10 16:37:57 +0530

    Additional config

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+// export the interface so it can be imported by other packages
+export interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+// give the constant a less‑generic name if you like
+const defaultColor: Color = {
+    red: 10,
+    blue: 10,
+    green: 10
+};
+
+export default defaultColor;
