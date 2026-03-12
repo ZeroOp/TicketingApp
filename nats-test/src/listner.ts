5cc6d422a8f88ccf33126de3d1a90367f9b84461
Zeroop <jayaramnaik725@gmail.com>
2026-03-12 21:22:34 +0530

    Configured nats event service to process the events with a queue group with setDurableName option and with setDeliverAllAvailable

--- nats-test/src/listner.ts
+++ nats-test/src/listner.ts
@@ -17,9 +17,13 @@ stan.on('connect', () => {
 
     const options = stan.subscriptionOptions()
         .setManualAckMode(true) // maual acknoledgement is set to true. if anything goes wrong in the message subscription. We need to ack till some time. 
+        .setDeliverAllAvailable() // every time we restart or lounch a service , we need the all messages delivered to this service. 
+        .setDurableName('order-service') // this will make sure one service should have a duriable subscription. when ever a service goes down , nats will dump the history of durable subscription. // they won't be coming back. 
+
+        // we want to add the queue group for sure with setDeliverAllAvailable option, with setDurableName along with Queue Group to persist our behaviour. 
     const subscription = stan.subscribe(
-        'ticket:created' ,
-        'LinsterQueueGroup',
+        'ticket:created',
+        'Queu-group',
         options
     );
 
