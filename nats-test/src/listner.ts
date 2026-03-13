47597be3d811d84408104521c7b43d599617b181
Zeroop <jayaramnaik725@gmail.com>
2026-03-13 16:51:06 +0530

    Added Listner abstract class

--- nats-test/src/listner.ts
+++ nats-test/src/listner.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming'
+import nats, { Message, Stan } from 'node-nats-streaming'
 import { randomBytes } from 'node:crypto';
 
 console.clear();
@@ -41,3 +41,50 @@ stan.on('connect', () => {
 // these signals will be differ for os. (windows / mac / linux)
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', ()=> stan.close());
+
+
+abstract class Listener {
+    abstract subject: string;
+    abstract queueGroupName: string;
+    abstract onMessage(data: any, msg: Message): void;
+    private client : Stan;
+    protected ackWait = 5*1000; // 5 seconds.
+
+    constructor(client : Stan) {
+        this.client = client;
+    }
+
+    subscriptionOptionis() {
+        return this.client.subscriptionOptions()
+                          .setDeliverAllAvailable()
+                          .setManualAckMode(true)
+                          .setAckWait(this.ackWait)
+                          .setDurableName(this.queueGroupName)
+    }
+
+    listen() {
+        const subscription = this.client.subscribe(
+            this.subject,
+            this.queueGroupName,
+            this.subscriptionOptionis()
+        )
+
+        subscription.on('message' , (msg: Message) => {
+            console.log(
+                `Message recived: ${this.subject} / ${this.queueGroupName}`
+            );
+
+            const parseData = this.parseMessage(msg);
+
+            this.onMessage(parseData, msg);
+        })
+    }
+
+    parseMessage(msg: Message) {
+        const data = msg.getData();
+
+        return typeof data === 'string'
+            ? JSON.parse(data)
+            : JSON.parse(data.toString('utf-8'))
+    }
+}
