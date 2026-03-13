3cfc99f38f6a4b6b27a6fe78aa81f8f1d70af8ac
Zeroop <jayaramnaik725@gmail.com>
2026-03-13 19:14:34 +0530

    Defined base-publisher and ticket-created-publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from '../events/ticket-created-publisher';
 
 console.clear()
 // stan is nats streaming service or you can call it as clients. 
@@ -7,17 +8,29 @@ const stan = nats.connect('ticketing', '123', {
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
-    console.log("publisher is connected to nats");
+stan.on('connect',async () => {
+    // console.log("publisher is connected to nats");
 
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
+    // const data = JSON.stringify({
+    //     id: '123',
+    //     title: 'concert',
+    //     price: 20
+    // });
 
-    stan.publish('ticket:created', data, ()=> {
-        console.log("Event published");
-    })
+    // stan.publish('ticket:created', data, ()=> {
+    //     console.log("Event published");
+    // })
+
+    const publisher = new TicketCreatedPublisher(stan)
+
+    try {
+      await publisher.publish({
+          id: '123',
+          title: 'conecrt',
+          price: 20
+      })
+    } catch(err) {
+      console.error(err);
+    }
 
 });
