4a1e0f6921a9d26eb2ec43d0c9138cdc04198ddb
Zeroop <jayaramnaik725@gmail.com>
2026-03-14 18:18:38 +0530

    Made nats-client into a singlotonClass

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator'
 import express, { Request, Response } from 'express'
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,7 +24,7 @@ router.post('/api/tickets', requireAuth, [
     })
     await ticket.save();
 
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
         id: ticket.id,
         title: ticket.title,
         price: ticket.price,
