b1fdbe33b435920581eb68722d9f51d8f1c27ee3
Zeroop <jayaramnaik725@gmail.com>
2026-03-15 15:41:05 +0530

    Setup order service successfully

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session'
+import { currentUser, errorHandler, NotFoundError } from '@zeroop-dev/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false  // in a test env we can make this false. 
+  })
+)
+
+app.use(currentUser); // we have added the current user middle ware to the application every single route in this project will be havingt this currenUser middleware ran in advance
+
+app.use(showTicketRouter)
+app.use(createTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
+// any route which is not found we are throughing the same error which is in the customError Format. 
+// made all the errors in the same format to make the client understand easily
+// if we have the async function then we need to use the next callback function like next(new NotFoundError()) instaead of using the throw new NotFoundError()
+app.use(() => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
