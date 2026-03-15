e56c47c30f296c976c17897348f10eb94574c2ee
Zeroop <jayaramnaik725@gmail.com>
2026-03-15 16:45:02 +0530

    Setup basic template for /api/orders to create and order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,17 @@
+import { currentUser, requireAuth, validateRequest } from '@zeroop-dev/common';
 import express, {Request, Response} from 'express'
-
+import { body } from 'express-validator';
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders',
+    requireAuth,
+    [
+        body('ticketId')
+            .not()
+            .isEmpty()
+            .withMessage('TicketId must be provided')
+    ]
+    , validateRequest, currentUser, async (req: Request, res: Response) => {
     res.send({});
 })
 
