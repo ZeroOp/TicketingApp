e72f4b2ddae16679ce7bd8b7cf326a2121772936
Zeroop <jayaramnaik725@gmail.com>
2026-03-16 16:22:12 +0530

    Completely setup new order creating route handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,11 @@
-import { currentUser, requireAuth, validateRequest } from '@zeroop-dev/common';
+import { BadRequestError, currentUser, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@zeroop-dev/common';
 import express, {Request, Response} from 'express'
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/orders';
 const router = express.Router();
+
+const EXIPRATION_WINDOW_SECONDS = 15*60;
 
 router.post('/api/orders',
     requireAuth,
@@ -12,7 +16,39 @@ router.post('/api/orders',
             .withMessage('TicketId must be provided')
     ]
     , validateRequest, currentUser, async (req: Request, res: Response) => {
-    res.send({});
+    
+    const { ticketId } = req.body;
+    // Find the ticket the user is tyring to order in the database. 
+    const ticket = await Ticket.findById(ticketId);
+
+    if(!ticket) {
+        throw new NotFoundError();
+    }
+    
+    // Make sure that this ticket is not already reserved
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+        throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date to this order. 
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXIPRATION_WINDOW_SECONDS)
+
+    //Build the ordre and save it to the database. 
+
+    const order = Order.build({
+        userId : req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket: ticket
+    })
+
+    await order.save();
+    // Publish an event saying that an order was created
+    res.status(201).send(order);
 })
 
 export { router as newOrderRouter };
