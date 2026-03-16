92df4542d9d628d7456ef32a0bd86917ae5c38ee
Zeroop <jayaramnaik725@gmail.com>
2026-03-16 21:57:57 +0530

    Added fetch order by id in show.ts route handler

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,19 @@
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@zeroop-dev/common';
 import express, {Request, Response} from 'express'
+import { Order } from '../models/orders';
 
 const router = express.Router();
 
-router.get('/api/orders/:id', async (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders/:id', requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id).populate('ticket');
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId != req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    return res.send(order);
 })
 
 export { router as showOrderRouter };
