27fc880aacd87d5bc6920256226a1abc4d00f6dc
Zeroop <jayaramnaik725@gmail.com>
2026-03-20 15:37:17 +0530

    Added order created listner

---
+++ tickets/src/events/listners/__test__/order-created-listner.test.ts
@@ -0,0 +1,57 @@
+import { OrderCreatedEvent, OrderStatus } from "@zeroop-dev/common";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper"
+import { OrderCreatedListner } from "../order-created-listner"
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+    // create an instance of the listner
+    const listner = new OrderCreatedListner(natsWrapper.client);
+
+    // Create and save a ticket
+
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 99,
+        userId: 'asdf'
+    })
+    await ticket.save();
+
+    // Create the fake data event
+    const data: OrderCreatedEvent['data'] = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        status: OrderStatus.Created,
+        userId: 'asdlfkjed',
+        expiresAt: 'alsdkfj',
+        ticket: {
+            id: ticket.id,
+            price: ticket.price
+        }
+    }
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    }
+    return { listner, ticket, data , msg }
+}
+
+it('sets the userId of the ticket', async () => {
+    const { listner, ticket, data, msg } = await setup();
+
+    await listner.onMessage(data, msg);
+
+    const updatedTicket = await Ticket.findById(ticket.id);
+
+    expect(updatedTicket!.orderId).toEqual(data.id);
+})
+
+it('acks the message', async () => {
+    const { listner, ticket, data, msg } = await setup();
+
+    await listner.onMessage(data, msg);
+    expect(msg.ack).toHaveBeenCalled();
+})
+
---
+++ tickets/src/events/listners/order-created-listner.ts
@@ -0,0 +1,28 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@zeroop-dev/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
+
+export class OrderCreatedListner extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName: string = queueGroupName;
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        // Find the ticket that the order is reserving
+        const ticket = await Ticket.findById(data.ticket.id);
+        // If not ticket, throw error
+        if (!ticket){
+            throw new Error('Ticket not found!');
+        }
+        // Mark the ticket as begin reserved by setting its orderId property
+        ticket.set({ orderId: data.id });
+        // save the ticket
+        await ticket.save();
+
+        new TicketUpdatedPublisher(natsWrapper.client)
+
+        // ack the message
+        msg.ack();
+    }
+}
